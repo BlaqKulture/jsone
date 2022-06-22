270740bacd22507797aab3dad7c4ef80bb7415b5
BlaqKulture <taiwovictor510@gmail.com>
2022-06-22 23:05:28 +0100

    Javascript Task

---
+++ index.js
@@ -0,0 +1,6 @@
+let Name = 'Taiwo Omobolanle'
+let Height = 65
+let Country = 'Nigeria'
+alert('I am'    +     Height     +    'Tall')
+alert('I am from'   +   Country)
+alert("My name is"   +   Name)
